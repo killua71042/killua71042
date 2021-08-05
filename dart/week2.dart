8db7fcd2b4ad0b4011b6dd8f431a4d2148cc5062
akkarapol <killua71042@gmail.com>
2021-08-05 17:33:37 +0700

    week2

--- dart/week2.dart
+++ dart/week2.dart
@@ -4,20 +4,23 @@ void main(){
    //var x = callColor("white");
    //print(x);
    
-   callWeb("Google", protocal : "https", port : 443);
+   callWeb("Google", protocal : "https", port : "443");
 }
-
+//ข้อที่ 1
 String callGen(int year) => (year >= 1996) ? "Gen Z" : "Gen Y";
 
+//ข้อที่ 2              สีที่ต้องการเพิ่มเข้าไป
 int callColor([String nemColor = "pink"]){
+    //เพิ่มสีตัวถัดไป
     List<dynamic> color = ["red", "blue", "green"];
     color.add(nemColor);
     for (var item in color){
         print(item);
     }
+    //นับจำนวนสี
     return color.length;
 }
-
-void callWeb(String web, {int port = 80, String protocal = "http"}) => {
-    print($protocal $web : $port);
-}
+//ข้อที่ 3         ชื่อเว็บ             port          protocal
+void callWeb(String web, {String port = "80", String protocal = "http"}) => 
+//   โชว์    protocal  ชื่อเว็บ  port
+    print("$protocal $web : $port");
